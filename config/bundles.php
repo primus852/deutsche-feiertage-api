a47260c4e9e3fc04b54d35b6f046dd09121293c0
Torsten <tw@mitscom.de>
2018-10-30 10:24:29 +0100

    Added MainPage

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
